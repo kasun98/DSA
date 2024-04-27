d67a811898cff99c4cc6e59c54069a1d4a178a2f
kasun98 <dewaka.lk@gmail.com>
2024-04-27 13:16:08 +0530

    maxloot

--- 13_max_loot.py
+++ 13_max_loot.py
@@ -6,29 +6,26 @@ def main():
     for _ in range(a):
         c = list(map(int, input().split()))
         comp.append(c)
-
+    
     print(f"{max_loot(a, n, comp):.4f}")
-
     
-
-    
-
 
 def max_loot(a, n, comp):
+    weights = [comps[1] for comps in comp]
+    frac = [comps[0]/comps[1] for comps in comp]
     totalValue = 0
+    
     for i in range(a):
-        if n == 0:
+        if n==0:
             return totalValue
-        else:
-            f = min(comp[i][1], n)
-            totalValue = totalValue + f*(comp[i][0]/comp[i][1])
-            #comp[i][1] -= f 
-            n = n - f
-            print(f, totalValue, n)
-
+        max_index = max(enumerate(frac), key=lambda x: x[1])[0]
+        f = min(weights[max_index], n)
+        totalValue += f*frac[max_index]
+        n = n-f
+        weights.pop(max_index)
+        frac.pop(max_index)
+        i += 1 
     return totalValue
-
-
 
 
 if __name__=='__main__':
